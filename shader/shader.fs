5989eaef5038d8ac11d1e0476d5e41af0b7e5834
parthishere <parthishere1234@gmail.com>
2026-08-19 02:01:27 -0700

    Make sure only the happy face looks in the other/reverse direction by changing the fragment shader

--- shader/shader.fs
+++ shader/shader.fs
@@ -9,6 +9,6 @@ uniform sampler2D texture2;
 
 void main()
 {   
-    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
+    FragColor = mix(texture(texture1, vec2(TexCoord.x, TexCoord.y)), texture(texture2, vec2(-TexCoord.x, TexCoord.y)), 0.2);
     // FragColor = texture(texture1, TexCoord);
 }
